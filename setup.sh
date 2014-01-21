1bc00dce95939d6e6e00ba97b5a375614d4e2e0c
Joe Hitchens <joe@sleepless.com>
2014-01-20 18:14:29 -0800

    -

--- setup.sh
+++ setup.sh
@@ -3,25 +3,23 @@
 cd
 
 if ! test -e .ssh/id_rsa ; then
-        echo -n "GitHub user name? "
-        read user
-        echo -n "GitHub repo name? "
-        read repo
         ssh-keygen
         echo
-	echo "Add this deployment key to your repo:"
+        echo "Add this deployment key to your repo:"
         echo
         cat .ssh/id_rsa.pub
         echo
         echo "Type ENTER when done: "
-	read foo
+        read foo
 fi
 
 if ! test -e "$repo" ; then
-	git config --global user.name "aws"
-	git config --global user.email "aws@$host"
-	git clone "git@github.com:$user/$repo.git"
-	cd "$repo"
-	source setup.sh
+        git config --global user.name "aws"
+        git config --global user.email "aws@$host"
+        echo -n "GitHub user name? " ; read user
+        echo -n "GitHub repo name? " ; read repo
+        git clone "git@github.com:$user/$repo.git"
+        cd "$repo"
+        source setup.sh
 fi
 
