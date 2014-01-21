310561390c286001e36fc8bb2bda20a919f24bb7
Joe Hitchens <joe@sleepless.com>
2014-01-20 18:11:17 -0800

    -

--- setup.sh
+++ setup.sh
@@ -2,13 +2,26 @@
 
 cd
 
-if ! test -e .ssh/id_rsax ; then
-        echo -n "Repo? "
+if ! test -e .ssh/id_rsa ; then
+        echo -n "GitHub user name? "
+        read user
+        echo -n "GitHub repo name? "
         read repo
-        # ssh-keygen
-        echo "Put this in github deployment keys and re-run this script:\n"
+        ssh-keygen
+        echo
+	echo "Add this deployment key to your repo:"
+        echo
         cat .ssh/id_rsa.pub
-        echo "\nThen re-run this script."
-        exit 0;
+        echo
+        echo "Type ENTER when done: "
+	read foo
+fi
+
+if ! test -e "$repo" ; then
+	git config --global user.name "aws"
+	git config --global user.email "aws@$host"
+	git clone "git@github.com:$user/$repo.git"
+	cd "$repo"
+	source setup.sh
 fi
 
